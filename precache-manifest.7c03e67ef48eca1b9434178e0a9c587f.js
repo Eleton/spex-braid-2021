c4d7203b352faffd1f353cdf1ecc893319fc0766
Aiman Josefsson <aiman@Aimans-MBP.office.lan>
2020-03-28 14:33:54 +0100

    Updates

---
+++ precache-manifest.7c03e67ef48eca1b9434178e0a9c587f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10ff2363ac7ea605e16ed0491f9d327",
+    "url": "/spex-braid-2021/index.html"
+  },
+  {
+    "revision": "36f03c1b5645f078e34d",
+    "url": "/spex-braid-2021/static/css/main.1b790c80.chunk.css"
+  },
+  {
+    "revision": "045a3387198e97449315",
+    "url": "/spex-braid-2021/static/js/2.c651ef48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spex-braid-2021/static/js/2.c651ef48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f03c1b5645f078e34d",
+    "url": "/spex-braid-2021/static/js/main.840697cb.chunk.js"
+  },
+  {
+    "revision": "28d4b8b68feeec980f94",
+    "url": "/spex-braid-2021/static/js/runtime-main.15cdc110.js"
+  }
+]);
