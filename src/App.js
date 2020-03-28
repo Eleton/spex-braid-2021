09991fdd2b98e66420198efc4c72c38d97ef4804
Aiman Josefsson <aiman@Aimans-MBP.office.lan>
2020-03-28 16:53:39 +0100

    Add persistant data in localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Node from "./Node";
 import { rawData, data, length } from "./data";
@@ -6,6 +6,29 @@ import { rawData, data, length } from "./data";
 function App() {
   const [state, setState] = useState(data);
   const refs = useRef({});
+
+  useEffect(() => {
+    if (!localStorage.getItem("braidData")) {
+      const answers = JSON.stringify(state.map(d => d.value));
+      localStorage.setItem("braidData", answers);
+    } else {
+      const answers = JSON.parse(localStorage.getItem("braidData"));
+      const tempState = state.map((d, i) => {
+        if (d.node === "input") {
+          return { ...d, value: answers[i] };
+        } else {
+          return d;
+        }
+      });
+      setState(tempState);
+    }
+  }, []);
+
+  const updateStorage = (value, index) => {
+    const answers = JSON.parse(localStorage.getItem("braidData"));
+    answers[index] = value;
+    localStorage.setItem("braidData", JSON.stringify(answers));
+  };
 
   const onBackspace = index => e => {
     if (
@@ -25,6 +48,7 @@ function App() {
     const tempData = [...state];
     tempData[index].value = value;
     setState(tempData);
+    updateStorage(value, index);
     if (value.length === 1) {
       refs.current[index + 1].focus();
     }
